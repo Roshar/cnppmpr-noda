47d9996bd99007541b47dbb77b9dcd32cf7506e7
kmva <fa.kmva@gmail.com>
2021-10-14 16:22:38 +0300

    socket connection

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const express = require('express')
 const app = express()
+const {createServer} = require('http')
+const server = createServer(app)
+const socket = require("socket.io");
+
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:8080",
+    methods: ["GET", "POST"],
+    credentials: true
+  },
+  allowEIO3: true
+})
+
+
 const bodyParser = require('body-parser')
 const passport = require('passport')
 const cors = require('cors')
@@ -11,6 +25,22 @@ app.use(cors())
 require('./middleware/passport')(passport)
 const routes = require('./settings/routes')
 routes(app)
-app.listen(PORT, ()=> {
+
+server.listen(PORT, ()=> {
     console.log(`App listen on ${PORT}`)
+})
+
+io.on('connection', (socket) => {
+    console.log('user connected');
+    io.emit('CONNECT', 'msg');
+
+    socket.on('add_message', (msg, userId) => {
+      //TODO
+      //добавить сообщение в бд
+
+      //отправить сообщения в store
+        
+       io.emit('SOCKET_ADD_MESSAGE', msg)
+       console.log('new event from server', msg)
+    })
 })
