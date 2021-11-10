754726e88f330e5ab55ed50e91b76fabbdc96e3a
Roshar <rashid.batukaev@gmail.com>
2021-11-10 11:20:26 +0300

    10 nov 2021

--- settings/db.js
+++ settings/db.js
@@ -7,15 +7,15 @@ class DB {
         return mysql.createConnection({
 
             //ВНЕШНЯ БАЗА НА БЕГЕТ
-            host: "govzalla.beget.tech",
-            user: "govzalla_it_cnpp",
-            password: "2404141rA!!",
-            database: "govzalla_it_cnpp"
+            // host: "govzalla.beget.tech",
+            // user: "govzalla_it_cnpp",
+            // password: "2404141rA!!",
+            // database: "govzalla_it_cnpp"
 
-            // host: "localhost",
-            // user: "root",
-            // password: "1234567890",
-            // database: "srm"
+            host: "localhost",
+            user: "root",
+            password: "1234567890",
+            database: "srm"
 
 
 
